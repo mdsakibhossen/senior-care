66b241afb2d82042a0afdc3ed3d71d7f41b05121
mdsakibhossen <sakib.hossen0143@gmail.com>
2024-05-24 12:13:01 +0600

    Loading & Not Found Pages are Added

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <main className="flex justify-center items-center min-h-screen dark:bg-slate-900 dark:text-slate-100">
+            <h1 className='text-4xl'>Loading...</h1>
+        </main>
+    )
+}
+
+export default Loading
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <main className="flex justify-center items-center min-h-screen dark:bg-slate-900 dark:text-slate-100">
+      <h1 className='text-4xl'>Page Not Found</h1>
+    </main>
+  )
+}
+
+export default NotFound
